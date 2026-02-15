2110bc21eae8f79e24f597ef36a2e62f4501b14d
Steven Gouws <steven@stevengouws.com>
2026-02-15 16:44:31 +0000

    Fix leap keymaps after move to codeberg

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -20,7 +20,9 @@ return {
      dependencies = { "tpope/vim-repeat" },
      url = "https://codeberg.org/andyg/leap.nvim",
      config = function()
-       require("leap").add_default_mappings()
+--       require("leap").add_default_mappings()
+        vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+        vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
      end,
    },
    {
