a7fdc435340f94729b17d1173197d5fbd7dc24fb
Steven Gouws <steven@stevengouws.com>
2026-04-17 08:01:00 +0100

    Use OSC52 to yank over ssh

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,6 +11,20 @@ vim.opt.gdefault = true
 
 vim.g.mapleader = " "
 
+-- Use OSC52 to yank over ssh
+
+vim.g.clipboard = {
+  name = 'osc52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
 if vim.g.vscode then
     -- VSCode extension
     -- Auto-format on leaving insert mode in VSCode Neovim
