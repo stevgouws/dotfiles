fddf6b397f8cffe640f1e715ea1ba9496071cb04
Steven Gouws <steven@stevengouws.com>
2026-06-28 13:51:50 +0100

    nvim: split right true

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,6 +5,7 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.splitright = true
 
 vim.opt.clipboard = "unnamed"
 vim.opt.gdefault = true
