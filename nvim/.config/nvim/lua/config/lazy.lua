63242b3b62a5784908c6c88be3061ece129e67da
Steven Gouws <steven@stevengouws.com>
2026-06-30 18:38:09 +0100

    nvim: fix double leader binding

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -15,11 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- mapleader is set in init.lua before this file is loaded
 
 -- Setup lazy.nvim
 require("lazy").setup({
