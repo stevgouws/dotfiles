32e4a8a8a38457c874925af4aaeb73688a69892f
Steven Gouws <steven@stevengouws.com>
2026-07-07 13:39:56 +0100

    copy actual env files not examples

--- projects-setup/projects/fe-ui/.worktree-setup.sh
+++ projects-setup/projects/fe-ui/.worktree-setup.sh
@@ -5,10 +5,9 @@ MAIN_WORKTREE="$HOME/projects/fe-ui"
 
 pnpm install
 # pnpm local:init-app-config-and-dotenv-files
-cp "$MAIN_WORKTREE/packages/e2e/.env.e2e" packages/e2e/
 cp "$MAIN_WORKTREE/.env" .
 cp "$MAIN_WORKTREE/.env.iis" .
-cp "$MAIN_WORKTREE/example.appconfig.json" appconfig.json
-cp "$MAIN_WORKTREE/packages/e2e/.env.example.e2e" .env.e2e
-cp "$MAIN_WORKTREE/packages/test-utils/.env.example.test" .env.test
+cp "$MAIN_WORKTREE/appconfig.json" .
+cp "$MAIN_WORKTREE/packages/e2e/.env.e2e" packages/e2e/
+cp "$MAIN_WORKTREE/packages/test-utils/.env.test" packages/test-utils/
 pnpm -r --filter '!@vcs-ui2/app' --filter '!@vcs-ui2/user-guides' run build
