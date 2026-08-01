47342b4b45d98108516e5e7aa82ebc5033182172
Steven Gouws <steven@stevengouws.com>
2026-08-01 13:03:40 +0100

    nvim: leader fl copy line number

--- nvim/.config/nvim/lua/keymaps/leader-f.lua
+++ nvim/.config/nvim/lua/keymaps/leader-f.lua
@@ -44,3 +44,37 @@ vim.keymap.set("n", "<leader>fr", function()
 
   copy(relative_path, "relative path")
 end, { desc = "File: copy relative path" })
+
+local function copy_path_with_lines(mode)
+  local filepath = vim.fn.expand("%:p")
+  if filepath == "" then
+    vim.notify("Current buffer has no filepath", vim.log.levels.WARN)
+    return
+  end
+
+  local result
+  if mode == "v" then
+    local start_line = vim.fn.line("v")
+    local end_line = vim.fn.line(".")
+    if start_line > end_line then
+      start_line, end_line = end_line, start_line
+    end
+    if start_line == end_line then
+      result = filepath .. ":" .. start_line
+    else
+      result = filepath .. ":" .. start_line .. "-" .. end_line
+    end
+  else
+    result = filepath .. ":" .. vim.fn.line(".")
+  end
+
+  copy(result, "path with line")
+end
+
+vim.keymap.set("n", "<leader>fl", function()
+  copy_path_with_lines("n")
+end, { desc = "File: copy path with line number" })
+
+vim.keymap.set("v", "<leader>fl", function()
+  copy_path_with_lines("v")
+end, { desc = "File: copy path with line range" })
